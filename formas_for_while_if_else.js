a5fe583214a3cce6b6f6c616e54ea6011bd7c41f
gustmacena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 21:14:43 -0300

    Formas de usar for while if else

---
+++ formas_for_while_if_else.js
@@ -0,0 +1,28 @@
+
+//for(iniciador, condiçao, incremento ){
+//   execução de algo
+
+
+for(let i=1; i<=10; i++){
+    console.log("for=> " + 1)
+}
+
+////////////////////////////////////////////////
+
+let j = 1
+while(j <=10){
+    console.log("while" + j)
+    j++
+    
+}
+
+///////////////////////////////////////////
+
+let sexo = "Masc"
+if (sexo == "Masculino"){
+    console.log("Masculino")
+} else if (sexo == "feminino") {
+    console.log("Feminino")
+} else {
+    console.log("sexo nao reconhecido")
+}
